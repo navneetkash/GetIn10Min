96c5703f21b67d8ddb766dc65ee710379f36787a
Navneet Kashyap <navneetkashyap178527@gmail.com>
2025-09-30 11:31:33 +0530

    Logout started working

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,12 +5,12 @@ export const AppContext = createContext();
 
 export const AppContextProvider = ({children})=>{
     const navigate = useNavigate();
-    const [user, setuser] = useState(true)
+    const [user, setUser] = useState(true)
     const [isSeller, setIsSeller] = useState(false)
     const [showUserLogin, setShowUserLogin] = useState(false)
 
 
-    const value = {navigate, user, setuser, isSeller, setIsSeller, showUserLogin, setShowUserLogin}
+    const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin}
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
