7225b188832f2d7677ec14182a04d784b8368647
Navneet Kashyap <navneetkashyap178527@gmail.com>
2025-10-05 06:58:12 +0530

    Started with adding prodcuts page

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../context/AppContext'
 import ProductCard from '../components/ProductCard'
 
@@ -22,7 +22,7 @@ const AllProducts = () => {
             <p className='text-2xl font-medium uppercase'>All Products</p>
             <div className='w-16 h-0.5 bg-primary rounded-full'></div>
         </div>
-        <div>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6'>
             {filteredProducts.filter((product)=>product.inStock).map((product, index)=>(
                 <ProductCard key={index} product={product} />
             ))}
