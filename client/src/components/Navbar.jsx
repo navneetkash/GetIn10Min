fff297111734e489fd10b81fe90f427e072c6fe6
Navneet Kashyap <navneetkashyap178527@gmail.com>
2025-10-05 16:53:11 +0530

    Started Cart Page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { useAppContext } from '../context/AppContext'
 
 const Navbar = () => {
     const [open, setOpen] = React.useState(false)
-    const { user, setUser, setShowUserLogin, navigate, searchQuery, setSearchQuery } = useAppContext();
+    const { user, setUser, setShowUserLogin, navigate, searchQuery, setSearchQuery, getCartCount, getCartAmount } = useAppContext();
     const logout = async () => {
         setUser(null);
         navigate('/')
@@ -37,7 +37,7 @@ const Navbar = () => {
 
                 <div onClick={() => { navigate("/cart") }} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80' />
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
                 </div>
 
                 {!user ? (
@@ -55,11 +55,16 @@ const Navbar = () => {
                 )
                 }
             </div>
-
-            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
+            <div className='flex items-center gap-6 sm:hidden'>
+                <div onClick={() => { navigate("/cart") }} className="relative cursor-pointer">
+                    <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80' />
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+                </div>
+                <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="">
                 {/* Menu Icon SVG */}
                 <img src={assets.menu_icon} alt='menu' />
             </button>
+            </div>
 
             {open && (
                 <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
