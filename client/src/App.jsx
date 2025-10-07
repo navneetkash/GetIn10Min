3e3e8630095b1f19576357de662931fc5b8653a8
Navneet Kashyap <navneetkashyap178527@gmail.com>
2025-10-07 23:45:47 +0530

    Frontend completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer'
 import Login from './components/Login'
 import { useAppContext } from './context/AppContext'
@@ -14,18 +14,21 @@ import AddAddress from './pages/AddAddress'
 import MyOrders from './pages/MyOrders'
 import SellerLogin from './components/seller/SellerLogin'
 import SellerLayout from './pages/seller/SellerLayout'
+import AddProduct from './pages/seller/AddProduct'
+import ProductList from './pages/seller/ProductList'
+import Orders from './pages/seller/Orders'
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes("seller");
-  const {showUserLogin, isSeller} = useAppContext()
+  const { showUserLogin, isSeller } = useAppContext()
 
   return (
     <div className='text-default min-h-screen text-gray-700 bg-white'>
       {isSellerPath ? null : <Navbar />}
       {showUserLogin ? <Login /> : null}
       <Toaster />
-      <div className={`${isSellerPath} ? "" : "px:6 md:px-16 lg:px-24 xl:px-32"`}>
+      <div className={`${isSellerPath ? "" : "px:6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/products' element={<AllProducts />} />
@@ -34,8 +37,10 @@ const App = () => {
           <Route path='/cart' element={<Cart />} />
           <Route path='/add-address' element={<AddAddress />} />
           <Route path='/my-order' element={<MyOrders />} />
-          <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin />}>
-
+          <Route path='/seller' element={isSeller ? <SellerLayout /> : <SellerLogin />}>
+            <Route index element={isSeller ? <AddProduct /> : null} />
+            <Route path='product-list' element={<ProductList />} />
+            <Route path='orders' element={<Orders />} />
           </Route>
         </Routes>
       </div>
