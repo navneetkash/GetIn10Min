71793839ebf212389501544060258f7d7dfda67c
Navneet Kashyap <navneetkashyap178527@gmail.com>
2025-10-13 15:44:50 +0530

    API creations is mostly done

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,9 +4,9 @@ import authUser from '../middlewares/authUser.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/register', register)
-userRouter.post('/login', login)
-userRouter.get('/is-auth', authUser, isAuth)
-userRouter.get('/logout', authUser, logout)
+userRouter.post('/register', register);
+userRouter.post('/login', login);
+userRouter.get('/is-auth', authUser, isAuth);
+userRouter.get('/logout', authUser, logout);
 
 export default userRouter
--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@ import cors from 'cors';
 import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/userRoutes.js';
-import authUser from './middlewares/authUser.js';
+import sellerRouter from './routes/sellerRoutes.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000
 
 await connectDB()
+await connectCloudinary()
 
 // Allowed multiple origin
 const allowedOrigins = ['http://localhost:5173']
@@ -22,6 +25,9 @@ app.use(cors({origin: allowedOrigins, credentials: true}));
 
 app.get('/', (req, res) => res.send("API is working"))
 app.use('/api/user', userRouter)
+app.use('/api/seller', sellerRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`)
